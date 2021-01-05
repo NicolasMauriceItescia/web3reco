a13320de6daa1ce23c8a83e27897d14d5a11974d
Yegabebar <m.barbegayet@gmail.com>
2021-01-05 18:32:13 +0100

    Début création code HTML de base

--- index.php
+++ index.php
@@ -3,8 +3,24 @@
     <head>
         <meta charset="utf-8">
         <title>Péhachepet Market</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <link rel="stylesheet" type="text/css" href="./css/styles.css">
     </head>
     <body>
-    	
+    	<header>
+    		<h1>Péhachepet Market</h1>
+    	</header>
+    	<main>
+    		<article>
+    			<header>
+    				<ul>
+    					<li> </li>
+    					<li> </li>
+    					<li> </li>
+    				</ul>
+    			</header>
+    		</article>
+    	</main>
+    	<?php include_once("footer.html") ?>
     </body>
 </html>
