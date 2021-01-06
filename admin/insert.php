5cfb26e1f7e20f4a1460893a27df024d2274ef40
Yegabebar <m.barbegayet@gmail.com>
2021-01-06 23:06:02 +0100

    Commit avant merge

--- admin/insert.php
+++ admin/insert.php
@@ -1,5 +1,16 @@
 <?php 
-    
+    try{
+        require("../included/dbconnect.php");
+        $sql = "SELECT typeId, name FROM categorieprod";
+        $result = $dbh->query($sql);
+        $typesTable = "";
+        while ( ($entry = $result->fetch(PDO::FETCH_ASSOC)) != FALSE) {
+            $typesTable .= '<option value="'.$entry['typeId'].'">'.$entry['name'].'</option>';
+        }
+    }catch(Exception $e){
+        $e->getMessage();
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -15,7 +26,21 @@
     		<article>
     			<header class="sub-banner">
     				<h2>Add products to inventory</h2>
-    			</header>
+                </header>
+                <section>
+                    <div class="operation">
+                        <?php 
+                            require("../misc/formbuilder.php");
+                            $form = new formBuilder();
+                            echo $form->addBook($typesTable);
+                            
+                        ?>
+
+                    </div>
+                    <div class="result">
+
+                    </div>
+                </section>
     			<ul>
                     <li class="back"><a href="../admin.php">Previous page</a></li>
                 </ul>
--- misc/formbuilder.php
+++ misc/formbuilder.php
@@ -10,7 +10,7 @@ class formBuilder{
         //1 textfield, submit
         $output = "";
         $output .= "<form action='' method='GET'>";
-        $output .= "<input type='text' id='' name='namestring'>";
+        $output .= "<input type='text' id='' name='namestring' required>";
         $output .=  "<button type='submit'>Valider</button>";
         $output .=  "</form>";
 
@@ -34,7 +34,7 @@ class formBuilder{
         //1 textfield, submit
         $output = "";
             $output .= "<form action='' method='GET'>";
-            $output .= "<input type='text' id='' name='namestring'>";
+            $output .= "<input type='text' id='' name='namestring' required>";
             $output .=  "<button type='submit'>Valider</button>";
             $output .=  "</form>";
        
@@ -42,18 +42,22 @@ class formBuilder{
        return $output;
     }
     
-    public function addBook(){
+    public function addBook($typesTable){
         //Method: POST
         //5 textfields: nameProduct, description
         //1 <select> to choose between this or that book type: use getter method
         //1 checkbox for the fairtrade attribute, 1 select for the book type, submit
         $output = "";
-        foreach ($form as $line) {
-            $output .= "<form action='' method='POST'>";
-            $output .= "<input type='' id='' name=''>";
-            $output .=  "<button type='submit'>Valider</button>";
-            $output .=  "</form>";
-       }
+        $output .= "<form action='' method='POST'>";
+        $output .= "<label for='idProduct'>Product ID</label><input type='text' id='' name='idProduct' required><br>";
+        $output .= "<label for='nameProduct'>Product name</label><input type='text' id='' name='nameProduct' required><br>";
+        $output .= "<label for='description'>Description</label><input type='text' id='' name='description' required><br>";
+        $output .= "<label for='fairtrade'>Fair trade</label><input type='checkbox' id='' name='fairtrade' required><br>";
+        $output .= "<label for='types'>Book type</label><select name='types'>".echo $typesTable."</select><br>";
+        $output .= "<label for='stock'>Quantity</label><input type='text' id='' name='stock' required><br>";
+        $output .= "<label for='discount'>Discount</label><input type='text' id='' name='discount' required><br>";
+        $output .=  "<button type='submit'>Valider</button>";
+        $output .=  "</form>";
     
        return $output;
     }
