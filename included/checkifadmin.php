8c5be0ece145c19174effd5c7cdc6deed2f72439
Yegabebar <m.barbegayet@gmail.com>
2021-01-08 02:02:54 +0100

    Ajout commentaires partie sessions

--- included/checkifadmin.php
+++ included/checkifadmin.php
@@ -1,11 +1,12 @@
 <?php
+    //Start the session mechanism for the page calling this script
     session_start();
-    if(isset($_SESSION['connect'])){
-        if($_SESSION['usertype']!='admin'){
+    if(isset($_SESSION['connect'])){ //If an account logged in from index.php, continue
+        if($_SESSION['accounttype']!='admin'){ //Then if the account type is not an admin: redirect to index.php
             header('location: index.php');
             exit();
         }
-    }else{
+    }else{ //Else if the accounttype is not defined, redirect to index.php
         header('location: index.php');
         exit();
     }
